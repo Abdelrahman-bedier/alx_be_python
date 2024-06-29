879701a2c4691ea7271403214ee7e196fd4375e8
Abdelrahman-bedier <abdo.bedier@gmail.com>
2024-06-29 12:55:10 +0300

    readme and task 0

---
+++ control-flow/README.md
@@ -0,0 +1,1 @@
+This project dives into the world of control flow and loops, essential concepts for introducing logic into your Python programs. You’ll learn how to make decisions using conditional statements and automate repetitive tasks using loops.
---
+++ control-flow/weather_advice.py
@@ -0,0 +1,9 @@
+input_weather = input("What's the weather like today? (sunny/rainy/cold):")
+if input_weather == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif input_weather == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif input_weather == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
