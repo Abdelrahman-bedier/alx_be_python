99c9f7be58494d2d8ee27edf68f08ce569ace24a
Abdelrahman-bedier <abdo.bedier@gmail.com>
2024-07-13 21:41:10 +0300

    edit withdraw0.2

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,6 +1,6 @@
 class BankAccount:
     def __init__(self, account_balance):
-        self.account_balance = 50
+        self.account_balance = 0
 
     def deposit(self, amount):
         self.account_balance += amount
