a4d49720a5657d1cd794f1ce6b0d67af23e76088
Abdelrahman-bedier <abdo.bedier@gmail.com>
2024-07-14 20:35:43 +0300

    task 3

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,41 @@
+
+class Book:
+    def __init__(self, title, author):
+        self.__is_checked_out = False
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+    
+    def is_checked_out(self):
+        return self.__is_checked_out
+    
+    def check_out(self):
+        self.__is_checked_out = True
+    
+    def return_book(self):
+        self.__is_checked_out = False
+
+class Library:
+    def __init__(self):
+        self.__books = []
+    
+    def add_book(self, Book):
+        self.__books.append(Book)
+    
+    def list_available_books(self):
+        for book in self.__books:
+            if not book.is_checked_out():
+                print(book)
+
+    def check_out_book(self, title):
+        for book in self.__books:
+            if title == book.title:
+                book.check_out()
+                break
+
+    def return_book(self, title):
+        for book in self.__books:
+            if title == book.title:
+                book.return_book()
